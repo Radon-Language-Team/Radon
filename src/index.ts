11bbc8aae65ded89fb524575dbd495b23ae3a2d2
Marwin <marwin.eder@gmx.de>
2024-02-12 22:36:01 +0100

    Even more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
+import tokenize from './tokenization';
 
 const fileToRead: string | undefined = process.argv[2];
 console.log('fileToRead', fileToRead ?? 'No file to read');
 
 if (fileToRead) {
 
-  const path = join(__dirname, `../${fileToRead}`);
+  let path: string;
+
+  // const path = join(__dirname, `../${fileToRead}`);
+
+  try {
+
+    path = join(__dirname, `../${fileToRead}`);
+    console.log('path', path);
+
+  } catch (error) {
+
+    console.log('Error: ', error);
+    process.exit(1);
+
+  }
 
   if (!path) {
 
@@ -17,11 +32,14 @@ if (fileToRead) {
 
   const content = readFileSync(path, 'utf-8');
   console.log(`File content: \n\n${content}`);
+
+  console.log('Tokens: ', tokenize(content));
+
   process.exit(0);
 
 } else {
 
-  console.log('No file to read');
+  console.log('Incorrect usage: npm  run compile <file-to-compile>');
   process.exit(1);
 
 }
--- src/tokenization.ts
+++ src/tokenization.ts
@@ -5,16 +5,25 @@
  * Copyright (C) 2024 - Marwin Eder
  */
 
+// eslint-disable-next-line no-shadow
 enum TokenType {
-  _out,
-  int_literal,
-  semi_colon,
+  _return = '_return',
+  int_literal = 'int_literal',
+  semi_colon = 'semi_colon',
 }
 
 interface Token {
   type: TokenType;
   value?: string;
 }
+
+const isLetter = (char: string): false | RegExpMatchArray | null => {
+  return char.length === 1 && char.match(/[a-z]/i);
+};
+
+const isInt = (input: string | number): false | RegExpMatchArray | null => {
+  return input.toString().length === 1 && input.toString().match(/[0-9]/);
+};
 
 /**
  * Tokenizes the input.
@@ -26,12 +35,68 @@ interface Token {
  */
 const tokenize = (input: string): Token[] => {
 
-  for (const char of input) {
+  const tokens: Token[] = [];
+  let currentBuffer = '';
 
-    console.log('char', char);
+  for (let i = 0; i < input.length; i++) {
+
+    const char = input[i];
+
+    if (isLetter(char)) {
+
+      currentBuffer += char;
+      i++;
+
+      while (isLetter(input[i])) {
+
+        currentBuffer += input[i];
+        i++;
+
+      }
+
+      if (currentBuffer === 'return') {
+
+        tokens.push({ type: TokenType._return, value: currentBuffer });
+        currentBuffer = '';
+        continue;
+
+      } else {
+
+        throw new Error(`Unrecognized token: ${currentBuffer}`);
+
+      }
+
+    } else if (isInt(char)) {
+
+      currentBuffer += char;
+      i++;
+
+      while (isInt(input[i])) {
+
+        currentBuffer += input[i];
+        i++;
+
+      }
+
+      tokens.push({ type: TokenType.int_literal, value: currentBuffer });
+      currentBuffer = '';
+
+    } else if (char === ';') {
+
+      console.log(`Chars left: ${input.slice(i + 1)}`);
+      tokens.push({ type: TokenType.semi_colon, value: ';' });
+      
+    } else if (char === ' ') {
+
+      console.log('Space');
+      continue;
+
+    }
 
   }
 
+  return tokens;
+
 };
 
-export { tokenize };
+export default tokenize;
