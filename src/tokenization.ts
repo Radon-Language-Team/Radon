fec819eae46c783e1d0a9176867c44bb5f21a1b2
Marwin <marwin.eder@gmx.de>
2024-02-13 23:42:59 +0100

    Rewriting Tokenizer using classes

--- src/tokenization.ts
+++ src/tokenization.ts
@@ -7,23 +7,81 @@
 
 // eslint-disable-next-line no-shadow
 enum TokenType {
-  _return = '_return',
+  _return = 'return',
   int_literal = 'int_literal',
   semi_colon = 'semi_colon',
 }
 
 interface Token {
   type: TokenType;
+  line: number;
   value?: string;
 }
 
-const isLetter = (char: string): false | RegExpMatchArray | null => {
-  return char.length === 1 && char.match(/[a-z]/i);
+class Buffer {
+
+  private buffer: string;
+
+  constructor() {
+    this.buffer = '';
+  }
+
+  public append(char: string | number | undefined): void {
+    if (char) {
+      this.buffer += char;
+    }
+  }
+
+  public clear(): void {
+    this.buffer = '';
+  }
+
+  public get value(): string {
+    return this.buffer;
+  }
+}
+
+class CharacterStream {
+
+  private readonly src: string;
+  private index: number;
+
+  constructor(src: string) {
+    this.src = src;
+    this.index = 0;
+  }
+
+  public peek(offset: number = 0): string | undefined {
+    if (this.index + offset >= this.src.length) {
+      return undefined;
+    }
+    return this.src[this.index + offset];
+  }
+
+  public consume(): string | undefined {
+    if (this.index >= this.src.length) {
+      return undefined;
+    }
+    return this.src[this.index++];
+  }
+}
+
+const isAlnum = (char: string | undefined): false | RegExpMatchArray | null => {
+  if (char) {
+    return char.match(/[a-zA-Z0-9]/);
+  } else {
+    return false;
+  }
 };
 
-const isInt = (input: string | number): false | RegExpMatchArray | null => {
-  return input.toString().length === 1 && input.toString().match(/[0-9]/);
+const isInt = (input: string | number | undefined): boolean => {
+  if (input) {
+    return Number.isInteger(+input);
+  } else {
+    return false;
+  }
 };
+
 
 /**
  * Tokenizes the input.
@@ -36,58 +94,53 @@ const isInt = (input: string | number): false | RegExpMatchArray | null => {
 const tokenize = (input: string): Token[] => {
 
   const tokens: Token[] = [];
-  let currentBuffer = '';
+  const stream = new CharacterStream(input);
 
-  for (let i = 0; i < input.length; i++) {
+  const buffer = new Buffer();
+  let lineCount = 1;
 
-    const char = input[i];
+  while (stream.peek()) {
+    if (isAlnum(stream.peek())) {
 
-    if (isLetter(char)) {
+      buffer.append(stream.consume());
 
-      while (isLetter(input[i])) {
-
-        currentBuffer += input[i];
-        i++;
-
+      while (stream.peek() && isAlnum(stream.peek())) {
+        buffer.append(stream.consume());
       }
 
-      i--;
-
-      if (currentBuffer === 'return') {
-
-        tokens.push({ type: TokenType._return, value: currentBuffer });
-        currentBuffer = '';
-        continue;
-
-      } else {
-
-        throw new Error(`Unrecognized token: ${currentBuffer} - Syntax Error`);
-
+      if (buffer.value === 'return') {
+        tokens.push({ type: TokenType._return, line: lineCount });
+        buffer.clear();
       }
 
-    } else if (isInt(char)) {
+    } else if (isInt(stream.peek())) {
 
-      while (isInt(input[i])) {
+      buffer.append(stream.consume());
 
-        currentBuffer += input[i];
-        i++;
-
+      while (stream.peek() && isInt(stream.peek())) {
+        buffer.append(stream.consume());
       }
 
-      tokens.push({ type: TokenType.int_literal, value: currentBuffer });
-      currentBuffer = '';
-      i--;
-      continue;
+      tokens.push({ type: TokenType.int_literal, line: lineCount, value: buffer.value });
+      buffer.clear();
 
-    } else if (char === ';') {
+    } else if (stream.peek() === ';') {
 
-      tokens.push({ type: TokenType.semi_colon, value: ';' });
-      i++;
-      continue;
+      tokens.push({ type: TokenType.semi_colon, line: lineCount });
+      stream.consume();
 
-    } else if (char === ' ') {
+    } else if (stream.peek() === ' ') {
 
-      continue;
+      stream.consume();
+
+    } else if (stream.peek() === '\n') {
+      
+      stream.consume();
+      lineCount++;
+
+    } else {
+
+      throw new Error(`Unexpected character: ${stream.peek()}`);
 
     }
 
