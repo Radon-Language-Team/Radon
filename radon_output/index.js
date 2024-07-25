ad93b869fe523e0d71067b952285778787abb510
xTheFreeZe <marwin.eder@gmx.de>
2024-07-25 12:13:38 +0200

    raus da

---
+++ radon_output/index.js
@@ -0,0 +1,7 @@
+const x = 10 + 200; 
+const y = 20; 
+const a = 'Hello' + ' World!'; 
+const b = ' How are you?'; 
+const c = 'Hello World!' + ' How are you?'; 
+console.log(x + y); 
+console.log(c); 
