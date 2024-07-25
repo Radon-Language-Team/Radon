2c9af152e0db5181380f7fd149e8ae59335c46b0
xTheFreeZe <marwin.eder@gmx.de>
2024-07-25 12:48:59 +0200

    V_Rewrite

--- radon_output/index.js
+++
@@ -1,7 +0,0 @@
-const x = 10 + 200; 
-const y = 20; 
-const a = 'Hello' + ' World!'; 
-const b = ' How are you?'; 
-const c = 'Hello World!' + ' How are you?'; 
-console.log(x + y); 
-console.log(c); 
