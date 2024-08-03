a7c0a363006f5e6b0b0e859744e435cf5fdf9894
xTheFreeZe <marwin.eder@gmx.de>
2024-08-03 17:25:58 +0200

    Add windows bin build again

--- build_bin.sh
+++ build_bin.sh
@@ -5,6 +5,16 @@ EXECUTABLE_NAME="radon"
 # Create the output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
 
+# Build for Windows
+echo "Building for Windows..."
+v -os windows -o $OUTPUT_DIR/${EXECUTABLE_NAME}.exe $SRC_DIR
+
+if [ $? -eq 0 ]; then
+    echo "Windows build successful: $OUTPUT_DIR/${EXECUTABLE_NAME}.exe"
+else
+    echo "Windows build failed."
+fi
+
 echo "Building for Linux..."
 v -os linux -o $OUTPUT_DIR/$EXECUTABLE_NAME $SRC_DIR
 
