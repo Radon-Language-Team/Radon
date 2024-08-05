0351f705514f62005eee6c4a79dc8a0740b0e010
xTheFreeZe <marwin.eder@gmx.de>
2024-08-05 17:15:14 +0200

    Dynamic Shell building script

--- build_bin.sh
+++ build_bin.sh
@@ -1,25 +1,33 @@
 SRC_DIR="src/cmd/radon.v"
 OUTPUT_DIR="radon"
 EXECUTABLE_NAME="radon"
+USER_OS=$(uname)
 
-# Create the output directory if it doesn't exist
-mkdir -p $OUTPUT_DIR
+if [ "$USER_OS" = "Linux" ]; then
 
-# Build for Windows
-echo "Building for Windows..."
-v -os windows -o $OUTPUT_DIR/${EXECUTABLE_NAME}.exe $SRC_DIR
+    echo "Building for Linux..."
+    v -os linux -o $OUTPUT_DIR/$EXECUTABLE_NAME $SRC_DIR
 
-if [ $? -eq 0 ]; then
-    echo "Windows build successful: $OUTPUT_DIR/${EXECUTABLE_NAME}.exe"
+    if [ $? -eq 0 ]; then
+        echo "Linux build successful: $OUTPUT_DIR/$EXECUTABLE_NAME"
+    else
+        echo "Linux build failed."
+    fi
+
+elif [ "$USER_OS" = "Windows" ]; then
+
+    echo "Building for Windows..."
+    v -os windows -o $OUTPUT_DIR/$EXECUTABLE_NAME.exe $SRC_DIR
+
+    if [ $? -eq 0 ]; then
+        echo "Windows build successful: $OUTPUT_DIR/$EXECUTABLE_NAME.exe"
+    else
+        echo "Windows build failed."
+    fi
+
 else
-    echo "Windows build failed."
-fi
 
-echo "Building for Linux..."
-v -os linux -o $OUTPUT_DIR/$EXECUTABLE_NAME $SRC_DIR
+    echo "Unsupported OS: $USER_OS"
+    exit 1
 
-if [ $? -eq 0 ]; then
-    echo "Linux build successful: $OUTPUT_DIR/$EXECUTABLE_NAME"
-else
-    echo "Linux build failed."
 fi
