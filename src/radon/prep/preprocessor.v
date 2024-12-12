f20f9abd02a8ba8f967eb6eea50281834073b028
Marwin <marwin.eder@gmx.de>
2024-12-12 15:26:24 +0100

    Locate std lib now works in every location

--- src/radon/prep/preprocessor.v
+++ src/radon/prep/preprocessor.v
@@ -12,18 +12,23 @@ pub mut:
 
 pub fn preprocess(file_path string) string {
 	mut p := Preprocessor{
-		file_path:   file_path
+		file_path: file_path
 	}
 
-	p.start_prep(file_path)
+	p.start_prep(file_path) or {
+		println(term.red('radon_prep Error: Failed to start preprocessing'))
+		exit(1)
+	}
 	return p.new_content
 }
 
-fn (mut p Preprocessor) start_prep(file_path string) {
-	home_dir := os.home_dir()
-	core_path := '.config/radon/core.rad'
-	core_script_path := os.join_path(home_dir, core_path)
-	
+fn (mut p Preprocessor) start_prep(file_path string) ! {
+	// Find the path of the radon executable
+	radon_path := os.dir(os.executable())
+
+	// Find the path of the standard library
+	core_script_path := os.join_path(('${radon_path}/../src/std/core.rad'))
+
 	if !os.exists(core_script_path) {
 		println(term.red('radon_prep Error: Was not able to locate standard library'))
 		exit(1)
