30ab8c6563182056f4ecce103dd4829bbf7b7df6
xTheFreeZe <marwin.eder@gmx.de>
2025-09-26 13:09:23 +0200

    Clean up

--- src/radon/parser/parse_function.v
+++ src/radon/parser/parse_function.v
@@ -214,57 +214,6 @@ fn parse_function_args(mut app structs.App) []structs.Param {
 			arg_type = structs.Token{}
 			arg_name = structs.Token{}
 		}
-
-		// for i := 0; i < args_buffer.len; i += 2 {
-		// 	if i + 1 >= args_buffer.len {
-		// 		print_compile_error('Expected a type followed by a name', &app)
-		// 		exit(1)
-		// 	}
-
-		// 	tok_type := args_buffer[i]
-		// 	tok_name := args_buffer[i + 1]
-
-		// 	if tok_type.t_type == .comma || tok_name.t_type == .comma {
-		// 		print_compile_error('Unexpected comma in parameter list', &app)
-		// 		exit(1)
-		// 	}
-
-		// 	if !tok_type.t_type.is_data_type() {
-		// 		print_compile_error('Expected a type before the parameter name, got ` ${tok_type.t_value} `',
-		// 			&app)
-		// 		exit(1)
-		// 	}
-
-		// 	if tok_name.t_type != .variable {
-		// 		print_compile_error('Expected parameter name, got ` ${tok_name.t_value} `',
-		// 			&app)
-		// 		exit(1)
-		// 	}
-
-		// 	function_params << structs.Param{
-		// 		name:   tok_name.t_value
-		// 		p_type: tok_type.t_type
-		// 	}
-
-		// 	function_param := structs.VarDecl{
-		// 		name:          tok_name.t_value
-		// 		function_name: app.current_parsing_function
-		// 		value:         structs.Expression{
-		// 			value:       tok_name.t_value
-		// 			e_type:      tok_type.t_type.to_var_type()
-		// 			is_variable: true
-		// 		}
-		// 		is_mut:        false
-		// 		variable_type: tok_type.t_type.to_var_type()
-		// 	}
-
-		// 	app.all_variables << function_param
-
-		// 	// Skip comma if any
-		// 	if i + 2 < args_buffer.len && args_buffer[i + 2].t_type == .comma {
-		// 		i++
-		// 	}
-		// }
 	}
 	return function_params
 }
